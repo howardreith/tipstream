9758425c21a52d1289cf9ea8c603d6333dd38a1d
howardreith <howard.reith@gmail.com>
2019-01-13 16:35:37 -0500

    Add dynamic title changing on delete modal
    -Title of stream to be deleted now appears in the modal to delete

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,26 +1,45 @@
 import React from 'react'
 import Modal from '../Modal'
 import history from '../../history'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <div>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
-      </div>
-    </React.Fragment>
-  )
-  return (
-    <div>
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push('/')}
-      />
-    </div>
-  )
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+  renderActions() {
+    return (
+      <React.Fragment>
+        <div>
+          <button className="ui button negative">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </React.Fragment>
+    )
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?'
+    } else {
+      return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+    }
+  }
+  render () {
+    return (
+        <Modal
+          title="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+    )
+  }
 }
 
-export default StreamDelete
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamDelete)
