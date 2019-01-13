856ea4848a1161414bffce438361851c2d6f8eca
howardreith <howard.reith@gmail.com>
2019-01-13 16:17:54 -0500

    Make modal independent on delete
    -Make modal a general component that can be used by other components
    -Put delete functionality more in StreamDelete and pass it to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import history from '../history'
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
-      <div onClick={(event) => event.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete this stream?
-        </div>
-        <div className="action">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+    <div
+    onClick={props.onDismiss}
+    className="ui dimmer modals visible active"
+    >
+      <div
+      onClick={(event) => event.stopPropagation()}
+      className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="action">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Modal from '../Modal'
+import history from '../../history'
 
 const StreamDelete = () => {
-  return <div>
-    StreamDelete
-    <Modal />
-  </div>
+  const actions = (
+    <React.Fragment>
+      <div>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </div>
+    </React.Fragment>
+  )
+  return (
+    <div>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
+    </div>
+  )
 }
 
 export default StreamDelete
