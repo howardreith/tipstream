e609d16c6340d4ec270282ad9b1d19774241496b
howardreith <howard.reith@gmail.com>
2019-01-13 19:51:12 -0500

    Add condition for deployed app
    -Add conditional so api is set to heroku when deployed or localhost 3001
    when local

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,5 +1,17 @@
 import axios from 'axios'
 
+let apiUrl
+const apiUrls = {
+  production: 'https://tipstream-api.herokuapp.com/',
+  development: 'http://localhost:3001'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
 export default axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: apiUrl
 })
