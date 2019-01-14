82476573c92d0893d81b686a9b5cf8c72d8cda46
howardreith <howard.reith@gmail.com>
2019-01-13 19:05:51 -0500

    Updates

---
+++ precache-manifest.3927c3b6f9bba19663c10b866a5e13cb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d323b3ed2b1152e25d0",
+    "url": "/tipstream/static/js/runtime~main.7d323b3e.js"
+  },
+  {
+    "revision": "d1616b94a44960298d5d",
+    "url": "/tipstream/static/js/main.d1616b94.chunk.js"
+  },
+  {
+    "revision": "e2e252d702d9e4ac45e3",
+    "url": "/tipstream/static/js/1.e2e252d7.chunk.js"
+  },
+  {
+    "revision": "3ffa97acfd35013e265d016bf7453807",
+    "url": "/tipstream/index.html"
+  }
+];
